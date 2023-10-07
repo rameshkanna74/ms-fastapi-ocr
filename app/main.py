11257ceb39e296f3c74fdb97c4ecee778d9c0c6c
rameshkanna74 <rameshkanna725@gmail.com>
2023-10-07 13:00:32 +0530

    fastapi template

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import pathlib
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+BASE_DIR = pathlib.Path(__file__).parent
+
+app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+
+@app.get("/", response_class=HTMLResponse)
+def home_view(request: Request):
+    print(request)
+    return templates.TemplateResponse("home.html", {"request":request, "name":"kanna"})
